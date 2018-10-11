4aa8de80cdc05dcc9f52c6d7953564dd31b9dc63
Uzver <uzver@Uzver.local>
2018-10-11 10:52:25 +0300

    Return file if a request is made

--- server.py
+++ server.py
@@ -8,14 +8,25 @@ PORT=5000
 serverSocket.bind((HOST, PORT))
 serverSocket.listen(10)
 
+def get_file(file_name):
+        file = open(file_name, "r")
+        return file.read()
 
 def conn_handler(conn, addr):
+    response_success = 'HTTP/1.1 200 OK \n'
+    response_error = 'HTTP/1.1 404 ERROR NOT FOUND\n'
     data = conn.recv(1024)
-    parse_filename(data.decode())
+    file_name = parse_filename(data.decode())
+    try:
+        conn.sendto(response_success.encode() + get_file(file_name) ,addr)
+    except Exception as e:
+        conn.sendto(response_error.encode(),addr)
+    conn.close()
 
 def parse_filename(data):
     file_name = data.split("HTTP")[0].split(" /")[-1]
     print (file_name)
+    return file_name.replace(" ","")
 
 while True:
     print ("Waiting for a connection....")
